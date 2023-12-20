25c08cc830e48903770c92d62dd48af556893568
Jarkko Sakkinen <jarkko@kernel.org>
2023-12-20 15:18:07 +0200

    fix(tpmdd_qemu_x86_64): use absolute paths in start-qemu.sh
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>

--- board/tpmdd_qemu_x86_64/post-image.sh
+++ board/tpmdd_qemu_x86_64/post-image.sh
@@ -1,6 +1,10 @@
 #!/bin/env bash
 
-QEMU_BOARD_DIR="$(dirname "$0")"
+BOARD_DIR="$(dirname "$0")"
 
-cp "${QEMU_BOARD_DIR}/start-qemu.sh.in" "${BINARIES_DIR}/start-qemu.sh"
+sed \
+  -e "s|@HOST_DIR@|${HOST_DIR}|g" \
+  <"${BOARD_DIR}/start-qemu.sh.in" \
+  >"${BINARIES_DIR}/start-qemu.sh"
+
 chmod +x "${BINARIES_DIR}/start-qemu.sh"
